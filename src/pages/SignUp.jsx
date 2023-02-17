1e1a729ba50abfb1a3b9ba9b7075529d8e9effb3
Duc Cuong <cuongtu532@gmail.com>
2023-02-17 09:06:39 +0700

    fix Signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -87,7 +87,7 @@ function SignUp() {
                     <div className="flex flex-wrap -mx-3 mb-4">
                       <div className="w-full px-3">
                         <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="email">Work Email <span className="text-red-600">*</span></label>
-                        <input id="email" type="email" className="form-input w-full text-gray-300" placeholder="examle@gmail.com" required />
+                        <input id="email" type="email" className="form-input w-full text-gray-300" placeholder="example@gmail.com" required />
                       </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-4">
