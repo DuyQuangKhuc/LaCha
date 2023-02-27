d41d98b5755396dbcdc4b1b46f9f3b4494107b2f
whisleyhozion <khucduyquang1801@gmail.com>
2023-02-27 11:59:34 +0700

    add

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -2,6 +2,8 @@
 export const actionType = {
     SET_USER: 'SET_USER',
     SET_ITEMS: 'SET_ITEMS',
+    SET_CART_SHOW: "SET_CART_SHOW",
+    SET_CARTITEMS: "SET_CARTITEMS",
 }
 
 const reducer = (state, action) => {
@@ -14,10 +16,22 @@ const reducer = (state, action) => {
                 user: action.user,
             };
 
-        case actionType.SET_ITEMS:  
+        case actionType.SET_ITEMS:
             return {
                 ...state,
                 Items: action.Items,
+            };
+
+        case actionType.SET_CART_SHOW:
+            return {
+                ...state,
+                cartShow: action.cartShow,
+            };
+
+        case actionType.SET_CARTITEMS:
+            return {
+                ...state,
+                cartItems: action.cartItems,
             };
 
         default:
